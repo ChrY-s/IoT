c71a2980ecd4aefea53dac234525c063d9209cfb
ChrY-s <christian.embrione@fermi.mo.it>
2026-04-20 17:28:17 +0200

    Server online
    Server online funzionale
    Accende e spegne lampadina e mostra lo stato di quest'ultima

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <FS.h>
+#include <LittleFS.h>
+
+#include <ArduinoJson.h>
+
 #define ssid "TIM-83699574"                 // SSID
 #define password "VA89zqccfKWwdeCtXd6N"     // Password
 
-const char* apiUrl = "https://openapi.api.govee.com/router/api/v1/device/control";
+// state per lo stato
+// control per controllarla
+const char* apiUrl;
 const char* apiKey = "2e17fc45-fefc-4c85-a25e-da3afc2d56b5";
+
+WiFiClientSecure client;
+HTTPClient http;
+DynamicJsonDocument doc(4096);
+
+// Server asincrono alla porta 80
+AsyncWebServer server(80);
 
 // Comando che invio al server Govee
 String jsonPayload;
+// Stato lampadina
+String ledState;
 
 // Accendo lampada
-void on (){
+void on(void){
+  // Url per la richiesta
+  apiUrl = "https://openapi.api.govee.com/router/api/v1/device/control";
+
   // Corpo JSON
   jsonPayload = R"rawliteral(
     {
@@ -26,10 +47,14 @@ void on (){
         }
       }
     })rawliteral";
+
+    ledState = "ON";
 }
 
 // Spengo lampada
-void off (){
+void off(void){
+  apiUrl = "https://openapi.api.govee.com/router/api/v1/device/control";
+
   jsonPayload = R"rawliteral(
     {
       "requestId": "uuid",
@@ -43,9 +68,35 @@ void off (){
         }
       }
     })rawliteral";
+
+    ledState = "OFF";
+}
+
+// Prendo lo stato della lampada
+void state(void) {
+  apiUrl = "https://openapi.api.govee.com/router/api/v1/device/state";
+
+  jsonPayload = R"rawliteral(
+    {
+    "requestId": "uuid",
+    "payload": {
+        "sku": "H600D",
+        "device": "6A:1F:10:20:BA:A2:70:3C"
+    }
+  })rawliteral";
+}
+
+// Gestisco la richiesta HTTP
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE")
+    return ledState;
+  return String();
 }
 
 // Cambio colore
+/*
 void change_color (){
   String jsonPayload = R"rawliteral(
     {
@@ -61,7 +112,42 @@ void change_color (){
       }
     })rawliteral";
 }
+*/
 
+// Invio richieste HTTP
+void ask(String cmd) {
+  // Richieste HTTP
+  if (WiFi.status() == WL_CONNECTED) {    
+    // Azione che voglio fare 
+    if (cmd == "ON") on();
+    else if (cmd == "OFF") off();
+
+    // Inizio richiesta HTTP
+    // Imposto header
+    http.begin(client, apiUrl);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Govee-API-Key", apiKey);
+
+    // Ricevo risposta
+    int httpResponseCode = http.POST(jsonPayload);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    String response = http.getString();
+    Serial.println("Risposta:");
+    Serial.println(response);
+
+    // Se chiedo lo stato stampo lo stato
+    if (cmd == "STATE") {
+      Serial.println("\nStato: ");
+      Serial.println(ledState);
+    }
+
+    // Chuido connessione HTTP
+    http.end();
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -75,30 +161,39 @@ void setup() {
   }
   Serial.println("\nConnesso!");
 
-  WiFiClientSecure client;
   client.setInsecure();
 
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
 
-    http.begin(client, apiUrl);
-    http.addHeader("Content-Type", "application/json");
-    http.addHeader("Govee-API-Key", apiKey);
-
-    // Azione che voglio fare 
-    change_color();
-
-    int httpResponseCode = http.POST(jsonPayload);
-
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-
-    String response = http.getString();
-    Serial.println("Risposta:");
-    Serial.println(response);
-
-    http.end();
+  // Initialize LittleFS
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
   }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/style.css", "text/css");
+  });
+  // Accendo lampadina
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    ask("ON");
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+  // Spengo lampadina
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    ask("OFF");  
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
